c413cfa661caad6681ff42aa012214c2398d0905
L-Garneta <garnetamahes05@gmail.com>
2026-07-08 19:24:25 +0700

    susun model

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -3,8 +3,49 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Event extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'organization_id',
+        'category_id',
+        'created_by',
+        'judul',
+        'tema',
+        'deskripsi',
+        'tanggal',
+        'waktu',
+        'lokasi',
+        'kuota',
+        'poster',
+        'status'
+    ];
+
+    protected $casts = [
+        'tanggal' => 'date',
+        'waktu' => 'datetime:H:i'
+    ];
+
+    public function organization()
+    {
+        return $this->belongsTo(Organization::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function registrations()
+    {
+        return $this->hasMany(Registration::class);
+    }
 }
--- app/Models/Organization.php
+++ app/Models/Organization.php
@@ -3,8 +3,32 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Organization extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'nama_organisasi',
+        'deskripsi',
+        'logo',
+        'created_by'
+    ];
+
+    /*
+    |--------------------------------------------------------------------------
+    | Relationship
+    |--------------------------------------------------------------------------
+    */
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function events()
+    {
+        return $this->hasMany(Event::class);
+    }
 }
